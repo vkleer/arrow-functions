5c2395b4a338d54da7078b4302e5263f71906439
Vilayat Kleer <vkleer@hotmail.com>
2022-08-19 23:32:53 +0000

    Add test arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(2, 8)
+console.log(sum2)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hi there!');
 
+const sayHello = () => console.log('Hello!');
+sayHello();
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multi-line string!
+    </p>`
+)
+console.log(returnMultipleLines());
